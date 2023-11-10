65328e3c458262cbd967929b58f43802e068b9b1
cesaryraziel <cesario.fcf@hotmail.com>
2023-11-10 16:55:08 -0300

    removendo a pasta build

--- build/arrays.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let numeros = [2.5, 2, 0.1];
-let nomes = ['Jose', 'Antonio', 'Maria', 'Francys'];
-console.log(numeros);
-console.log(nomes);
--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let a = 10;
-let b = 5;
-let c = a + b;
-console.log(`A soma de a + b é ${c}`);
